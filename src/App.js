d33786614f080c65fb26d9518bcac4d8a5ac78cb
Sejal <sejal.dua@tufts.edu>
2019-10-09 13:37:43 +0200

    Added new passages

--- src/App.js
+++ src/App.js
@@ -19,12 +19,9 @@ class App extends React.Component {
 
 	setText = () => {
 		const texts = [
-			`You never read a book on psychology, Tippy. You didn\'t need to. You knew by some divine instinct that you can make more friends in two months by becoming genuinely interested in other people than you can in two years by trying to get other people interested in you.`,
-			`I know more about the private lives of celebrities than I do about any governmental policy that will actually affect me. I'm interested in things that are none of my business, and I'm bored by things that are important to know.`,
-			`A spider's body consists of two main parts: an anterior portion, the prosoma (or cephalothorax), and a posterior part, the opisthosoma (or abdomen).`,
-			`As customers of all races, nationalities, and cultures visit the Dekalb Farmers Market by the thousands, I doubt that many stand in awe and contemplate the meaning of its existence. But in the capital of the Sunbelt South, the quiet revolution of immigration and food continues to upset and redefine the meanings of local, regional, and global identity.`,
-			`Outside of two men on a train platform there's nothing in sight. They're waiting for spring to come, smoking down the track. The world could come to an end tonight, but that's alright. She could still be there sleeping when I get back.`,
-			`I'm a broke-nose fighter. I'm a loose-lipped liar. Searching for the edge of darkness. But all I get is just tired. I went looking for attention. In all the wrong places. I was needing a redemption. And all I got was just cages.`
+			`Let us imagine that you are in a race with a tortoise. The tortoise has a ten-yard head start. In the time it takes you to run that ten yards, the tortoise has moved one yard. And then in the time it takes you to make up that distance, the tortoise goes a bit farther, and so on forever. You are faster than the tortoise but you can never catch him; you can only decrease his lead.`,
+			`Plumbers have everything: greed, sex, spirituality, white-knuckled chases, shameful propositions, a nun, humor, true love, jaded love, taut action, comedy, a bad guy, a good guy, a hero, spine-tingling suspense, a hot babe, a damsel in distress, and a Hollywood ending!`,
+			`Well, a scavenger hunt is exactly like a treasure hunt, except in a treasure hunt you try to find something you want, and in a scavenger hunt you try to find something that nobody wants.`
 		];
 		const text = texts[Math.floor(Math.random() * texts.length)];
 		const words = text.split(' ');
@@ -56,6 +53,7 @@ class App extends React.Component {
 
 		// if space or '.', check the word
 		if (lastLetter === ' ' || lastLetter === '.') {
+
 			// check to see if it matches to the currentWord
 			// trim because it has the space
 			if (inputValue.trim() === currentWord) {
